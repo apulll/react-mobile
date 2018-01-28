5ed32c831bf4c6eb9547a77fb8d47b99c29bb773
capul <1216065136@qq.com>
2018-01-28 23:15:16 +0800

    登陆页逻辑添加

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,29 +1,38 @@
 import React from 'react';
-import { List, InputItem, Button, WingBlank, WhiteSpace  } from 'antd-mobile';
+import { List, InputItem, Button, WingBlank, WhiteSpace, Modal, Flex } from 'antd-mobile';
 import { Link } from 'react-router';
 import { createForm } from 'rc-form';
 import { isEmpty, assign } from 'lodash';
 import HNavBar from 'components/HNavBar';
-import { setDomainCookie } from 'pages/InterviewForm/util';
+import { setDomainCookie, defaultParams } from 'pages/InterviewForm/util';
 import fetch from 'utils/fetch';
 import './index.less';
 // import ENV from 'ENV';
+const FlexItem = Flex.Item
 
 class Home extends React.Component {
   
   constructor(props) {
     super(props)
     this.state = {  
+      visible:false,
       info:{
         company_name:'',
         fullName:'',
         mobile:'',
       },
       code:null,
+      captcha:null,
+      captchaCode:null,
+      snsDisabled:true,
+      // submit params
+      loading:false,
+      disabled:false,
     }
   }
   componentWillMount() {
     this.getInviteInfo()
+    this.getCaptcha()
   }
   componentDidMount(){
     // console.log(ENV,'env')
@@ -32,6 +41,7 @@ class Home extends React.Component {
     // !isEmpty(query) && this.setState({query})
     // getAccessToken()
   }
+
   getInviteInfo = async () =>{
     const query = this.props.location.query
     const url = `http://hrmapi.dev.mila66.com/Api/recruitment/interview/inviteInfoh5`
@@ -58,9 +68,38 @@ class Home extends React.Component {
       }
     }
   }
+  getCaptcha = async () =>  {
+    const url = `/localapi/getCaptcha`;
+    // const {mobile} = this.state;
+    try{
+      const newData = await fetch({
+                              url:url
+                            })
+      if(newData){
+          const data = newData.res
+          // console.log(data)
+          this.setState({captcha:data})
+        }
+    }catch(error){
+
+    }
+  }
+  changeCaptcha = (value)=> {
+
+    // this.state.captcha && 
+    if(value.toLowerCase() === this.state.captcha.text.toLowerCase()) {
+        this.setState({captchaCode:value,snsDisabled:false})
+
+    }else{
+      this.setState({captchaCode:null,snsDisabled:true})
+    }
+  }
   getCode = async (e)=> {
-    const url = `http://hrmapi.local.com/Api/interview/fill`;
-    const {mobile} = this.state;
+    this.setState({visible:true})
+  }
+  getSnsCode = async ()=> {
+    const url = `http://hrmapi.local.com/Api/interview/fill/sms`;
+    const {mobile} = this.state.info;
     try{
       const newData = await fetch({
                               url:url,
@@ -71,15 +110,48 @@ class Home extends React.Component {
                             })
       if(newData){
           const info = newData.res
-
+          
           // this.setState({code})
         }
     }catch(error){
 
     }
   }
+  onClose = () => {
+    this.setState({visible:false})
+  }
+  submitHandle = ()=> {
+    // const { detail } = this.state
+    // const { params } = this.props
+    this.props.form.validateFields({ force: true }, async (error, value) => {
+      this.setState({loading:true,disabled:true})
+      console.log(value,'value')
+      if(error){
+        // const errorMsg = formErrorsMsg(error)
+        // this.setState({errorMsg})
+        this.setState({loading:false,disabled:false})
+      }else {
+        // this.setState({errorMsg:''})
+        
+        const fixParams = defaultParams()
+        value = assign({}, value, fixParams)
+
+        try {
+          const newData = await fetch({
+                          url:`http://hrmapi.local.com/Api/interview/fill/login`,
+                          method:"post",
+                          data:value,
+                        })
+          this.setState({loading:false,disabled:false})
+        }catch(error){
+          this.setState({loading:false,disabled:false})
+        }
+        
+      }  
+    });
+  }
   render() {
-    const { info } = this.state;
+    const { info, visible, captcha, snsDisabled, loading, disabled } = this.state;
     const { getFieldProps } = this.props.form;
     console.log(this.state)
     return (
@@ -106,12 +178,14 @@ class Home extends React.Component {
       	  <InputItem
             {...getFieldProps('name',{
               initialValue: info.fullName,
+              rules: [{required: true, message: `不可为空`}],
             })}
             editable={false}
           >姓名</InputItem>
           <InputItem
             {...getFieldProps('mobile',{
               initialValue: info.mobile,
+              rules: [{required: true, message: `不可为空`}],
             })}
             editable={false}
           >手机号码</InputItem>
@@ -119,6 +193,7 @@ class Home extends React.Component {
           	type="digit"
             {...getFieldProps('code',{
               initialValue: '',
+              rules: [{required: true, message: `不可为空`}],
             })}
             extra={<Button type="primary" size="small" inline="true" onClick={this.getCode}>获取验证码</Button>}
           >验证码</InputItem>
@@ -126,7 +201,34 @@ class Home extends React.Component {
         </List>
         <WhiteSpace />
         <WhiteSpace />
-        <WingBlank><Link to="/interview"><span><Button type="primary" size="large">提交</Button></span></Link></WingBlank>
+        <WingBlank><span><Button type="primary"  onClick={this.submitHandle} loading={loading} disabled={disabled}>提交</Button></span></WingBlank>
+
+        <Modal
+          visible={visible}
+          transparent
+          maskClosable={true}
+          >
+            <div>
+              <Flex>
+                <FlexItem>
+                  <InputItem
+                    onChange={this.changeCaptcha}
+                    placeholder="请填写验证码"
+                    maxLength={4}
+                  />
+                </FlexItem>
+                <FlexItem>
+                  <div  onClick={this.getCaptcha}  dangerouslySetInnerHTML={{__html: captcha?captcha.data:''}} />
+                </FlexItem>
+              </Flex>
+              <WhiteSpace />
+              <Button onClick={this.getSnsCode} type="primary" disabled={snsDisabled}>获取短信验证码</Button>
+              <WhiteSpace />
+              <Button onClick={this.onClose}>关闭</Button>
+            </div>
+        </Modal>
+
+        
       </div>
       </div>
     );
